926b0ede1eaf17faf6490e541c7383cfacc2a84f
Aurelio Guerrero <aurelio.guerrero@gmail.com>
2012-12-03 10:40:51 -0500

    intento de guardado30

--- js/eventos.js
+++ js/eventos.js
@@ -2,6 +2,7 @@
 var rutaRaiz = null;
 var archivoEntry = null;
 var directorioEntry = null;
+var directorioRoot = null;
 
 function capturarFoto() 
 {
@@ -33,41 +34,9 @@ function errorArchivo(error)
 	alert('Error al guardar archivo: '+error.code);
 }
 
-function guardarFoto()
+function archivoGuardado(entry)
 {
-	alert('Función guardarFoto');
-	window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, intentarGuardado, errorArchivo);
-}
-
-function intentarGuardado(fileSystem)
-{
-	try
-	{
-		var rutaArchivo = document.getElementById('imagen').src;
-		var nombre = document.getElementById('nombrearchivo').value;	
-		alert('Ruta temporal: '+rutaArchivo);
-		alert('Nombre Archivo: '+nombre);
-		
-		rutaRaiz = fileSystem.root.fullPath;
-		alert('Ruta root: '+rutaRaiz);
-		alert('Ruta archivo: '+rutaArchivo.substring(rutaRaiz.length+1));
-		
-		fileSystem.root.getFile(rutaArchivo.substring(rutaRaiz.length+1), {create: false, exclusive: false}, obtenerArchivo, errorArchivo);
-		fileSystem.root.getDirectory('Album Fotos',{create: true, exclusive: false}, obtenerNuevoDir, errorArchivo);
-		/*alert('archivo antes de mover: '+archivoEntry.name);
-		alert('va hacia: '+directorioEntry.name);*/
-		archivoEntry.moveTo(directorioEntry, nombre+'.jpg', archivoGuardado, errorArchivo);
-	}
-	catch(err)
-	{
-		alert("error: "+err);
-	}	
-}
-
-function obtenerArchivo(fileEntry)
-{	
-	archivoEntry = fileEntry;
-	alert('Se obtiene el archivo!'+archivoEntry.name);
+	alert("Se ha guardado el archivo en la ruta: "+entry.name);
 }
 
 function obtenerNuevoDir(directoryEntry)
@@ -76,6 +45,7 @@ function obtenerNuevoDir(directoryEntry)
 	{
 		directorioEntry = directoryEntry;
 		alert('Se crea el directorio!'+directorioEntry.name);
+		archivoEntry.moveTo(directorioEntry, nombre+'.jpg', archivoGuardado, errorArchivo);
 	}
 	catch(err)
 	{
@@ -83,7 +53,37 @@ function obtenerNuevoDir(directoryEntry)
 	}
 }
 
-function archivoGuardado(entry)
+function obtenerArchivo(fileEntry)
+{	
+	archivoEntry = fileEntry;
+	alert('Se obtiene el archivo!'+archivoEntry.name);
+	directorioRoot.root.getDirectory('Album Fotos',{create: true, exclusive: false}, obtenerNuevoDir, errorArchivo);
+}
+
+function intentarGuardado(fileSystem)
 {
-	alert("Se ha guardado el archivo en la ruta: "+entry.name);
+	try
+	{
+		directorioRoot = fileSystem;
+		var rutaArchivo = document.getElementById('imagen').src;
+		var nombre = document.getElementById('nombrearchivo').value;	
+		alert('Ruta temporal: '+rutaArchivo);
+		alert('Nombre Archivo: '+nombre);
+		
+		rutaRaiz = directorioRoot.root.fullPath;
+		alert('Ruta root: '+rutaRaiz);
+		alert('Ruta archivo: '+rutaArchivo.substring(rutaRaiz.length+1));
+		
+		directorioRoot.root.getFile(rutaArchivo.substring(rutaRaiz.length+1), {create: false, exclusive: false}, obtenerArchivo, errorArchivo);		
+	}
+	catch(err)
+	{
+		alert("error: "+err);
+	}	
+}
+
+function guardarFoto()
+{
+	alert('Función guardarFoto');
+	window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, intentarGuardado, errorArchivo);
 }
